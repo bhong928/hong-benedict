4e3f942556756d8e592a0ca699bcbd30e617e6d8
Ben Hong <benhong928@gmail.com>
2025-06-26 14:26:35 -0500

    text color

--- src/components/Education/page.jsx
+++ src/components/Education/page.jsx
@@ -3,7 +3,7 @@ export default function Education() {
 
     return(
         <section className="p-20 bg-white">
-            <h2 className="text-3xl font-bold mb-12 text-gray-600">Education</h2>
+            <h2 className="text-3xl font-bold mb-12 text-gray-700">Education</h2>
             <div className="relative space-y-12 ml-6 border-l-2 border-gray-400">
 
                 {/* University of Michigan */}
