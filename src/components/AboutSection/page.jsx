29b346e64831b861d42c66239a602f2a676b64a4
Ben Hong <benhong928@gmail.com>
2025-07-31 10:09:48 -0500

    feat: style and structure updates to About Me Section added light design enhancements for improved readability adjusted layout and spacing for better visual flow fine tuned typography and responsiveness

--- src/components/AboutSection/page.jsx
+++ src/components/AboutSection/page.jsx
@@ -1,49 +1,54 @@
 'use client'; // Needed if you're using Next.js App Router
 
-import { motion } from 'framer-motion';
+import { motion, useTransform } from 'framer-motion';
 import { Instagram, Linkedin, GitHub } from 'react-feather';
 
 export default function AboutSection(){
 
+    const fadeUp = {
+        initial: { opacity: 0, y: 50 },
+        whileInView: { opacity: 1, y: 0 },
+        viewport: { once: true },
+        transition: { duration: 1, ease: 'easeOut' },
+
+    }
+
     return(
         <motion.section
             id="about"
-            className="bg-black px-6 py-16"
-            initial={{ opacity: 0, y: 50 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            viewport={{ once: true }}
-            transition={{ duration: 0.6, ease: 'easeOut' }}
+            className="bg-[#2c2c2c] px-6 py-16"
+            {...fadeUp}
         >
-            <div className="flex flex-col md:flex-row justify-between gap-10 max-w-7xl mx-auto items-center">
+            <div className="flex flex-col lg:flex-row justify-between gap-20 items-center">
                 {/* Personal Info on the left */}
-                <div className="flex-1 space-y-4">
+                <div className="flex-1 space-y-4 max-w-3xl">
                     {/* Info */}
-                    <div className="space-y-4">
+                    <div className="space-y-4 w-full text-[4vw] md:text-[2vw] lg:text-[1vw]">
 
-                        <div className="flex justify-between border-b border-gray-300 pb-2">
-                            <span className="text-white font-semibold">Name:</span>
-                            <span className="text-gray-500">Benedict Hong</span>
+                        <div className="flex justify-between border-b border-gray-300 pb-2 uppercase">
+                            <span className="text-gray-400 font-semibold">Name:</span>
+                            <span className="text-gray-200">Benedict Hong</span>
                         </div>
 
-                        <div className="flex justify-between border-b border-gray-300 pb-2">
-                            <span className="text-black font-semibold">Age:</span>
-                            <span className="text-gray-500">26</span>
+                        <div className="flex justify-between border-b border-gray-300 pb-2 uppercase">
+                            <span className="text-gray-400 font-semibold">Age:</span>
+                            <span className="text-gray-200">26</span>
                         </div>
 
-                        <div className="flex justify-between border-b border-gray-300 pb-2">
-                            <span className="text-black font-semibold">Location:</span>
-                            <span className="text-gray-500">Dallas, Texas</span>
+                        <div className="flex justify-between border-b border-gray-300 pb-2 uppercase">
+                            <span className="text-gray-400 font-semibold">Location:</span>
+                            <span className="text-gray-200">Dallas, Texas</span>
                         </div>
 
-                        <div className="flex justify-between border-b border-gray-300 pb-2">
-                            <span className="text-black font-semibold">School:</span>
-                            <span className="text-gray-500">University of Michigan</span>
+                        <div className="flex justify-between border-b border-gray-300 pb-2 uppercase">
+                            <span className="text-gray-400 font-semibold">School:</span>
+                            <span className="text-gray-200">University of Michigan</span>
                         </div>
 
                     </div>
 
                     {/* Socials */}
-                    <div className="flex space-x-6 text-gray-700 text-2xl">
+                    <div className="flex space-x-6 text-gray-200">
                         <a href="https://www.instagram.com/_benedict_hong/" target="_blank" rel="noopener noreferrer">
                             <Instagram className="hover:text-blue-300 " />
                         </a>
@@ -57,29 +62,29 @@ export default function AboutSection(){
                 </div>
 
                 {/* Right: Bio + Resume */}
-                <div className="flex-1 space-y-6">
-                <div className="text-gray-500">
-                    <h2 className="text-2xl font-semibold mb-2">A little bit about me...</h2>
-                    <p className="mb-4">
-                    I am a recent Software Engineering graduate from the University of Michigan with a passion for building clean, user-friendly web applications. I enjoy solving real world problems through code and have experience with technologies like React, Next.js, Tailwind CSS, Python, and Firebase. 
-                    </p>
-                    
-                    <p className="mb-4">
-                    Previously, I worked as an Applications Engineer Intern at Veoneer, where I contributed to the development and testing of airbag modules and sensors for major OEMs. That expereience taught me the importance of percision, collaboration, and building systems that prioritize safety and reliability.
-                    </p>
+                <div className="flex-1 space-y-20 text-[4vw] md:text-[2vw] lg:text-[1vw] text-center">
+                    <div className="text-gray-200">
+                        <h2 className="text-2xl font-semibold mb-2 upper">A little bit about me...</h2>
+                        <p className="mb-4">
+                        I am a recent Software Engineering graduate from the University of Michigan with a passion for building clean, user-friendly web applications. I enjoy solving real world problems through code and have experience with technologies like React, Next.js, Tailwind CSS, Python, and Firebase. 
+                        </p>
+                        
+                        <p className="mb-4">
+                        Previously, I worked as an Applications Engineer Intern at Veoneer, where I contributed to the development and testing of airbag modules and sensors for major OEMs. That expereience taught me the importance of percision, collaboration, and building systems that prioritize safety and reliability.
+                        </p>
 
-                    <p className="mb-4">
-                    Outside of coding, I am a Musician, Mix/Mastering Engineer, and creative thinker who enjoys collaborating with others to bring ideas to life! 
-                    </p>
-                </div>
+                        <p className="mb-4">
+                        Outside of coding, I am a Musician, Mix/Mastering Engineer, and Creator who enjoys collaborating with others to bring ideas to life! 
+                        </p>
+                    </div>
 
-                <a 
-                    href='/BenedictHong_Resume.pdf'
-                    download
-                    className="border text-gray-500 px-6 py-3 hover:bg-blue-200 transition cursor-pointer font-bold"
-                >
-                    Download Resume
-                </a>
+                    <a 
+                        href='/BenedictHong_Resume.pdf'
+                        download
+                        className="border rounded-sm text-gray-200 px-6 py-3 hover:bg-blue-200 hover:text-gray-400 transition cursor-pointer font-bold"
+                    >
+                        Download Resume
+                    </a>
                 </div>
             </div>
         </motion.section>
--- src/components/HeroSection/page.jsx
+++ src/components/HeroSection/page.jsx
@@ -8,12 +8,29 @@ export default function HeroSection() {
   const ref = useRef(null);
   const { scrollY } = useScroll();
   const y = useTransform(scrollY, [0, 600], [0, 250]); // Adjust values as needed
+  const fadeRight = {
+    initial: { opacity: 0, x:-50 },
+    animate: { opacity: 1, x: 0 },
+    transition: { duration: 1 },
+  }
+  const fadeLeft = {
+    initial: { opacity: 0, x: 50 },
+    animate: { opacity: 1, x: 0 },
+    transition: { duration: 1 },
+    
+  }
+  const fadeIn = {
+    initial: { opacity: 0, y: 50 },
+    whileInView: { opacity: 1, y: 0 },
+    viewport: { once: true },
+    transition: { duration: 1, ease: 'easeOut' }
+  }
 
   return (
     <section
       ref={ref}
-      className="relative w-full overflow-hidden h-auto md:h-[60vh]
-                text-white md:text-black 
+      className="md:bg-[#F1F0EA] relative w-full overflow-hidden h-auto md:h-[60vh]
+                text-white md:text-[#2c2c2c]
                 bg-cover bg-center bg-no-repeat 
                 bg-[url('/images/me.jpg')] md:bg-none filter grayscale md:grayscale-0 md:filter-none"
       >
@@ -23,40 +40,40 @@ export default function HeroSection() {
           <div className='text-center'>
             <motion.h1 
               className='text-[10vw] font-black uppercase md:text-[5vw]'
-              initial = {{ opacity: 0, x:-50 }}
-              animate = {{ opacity: 1, x: 0 }}
-              transition = {{ duration: 1 }}
+              {...fadeRight}
             >
               BENEDICT
             </motion.h1>
             <motion.h1
               className="text-[10vw] md:text-[5vw] font-black uppercase"
-              initial={{ opacity: 0, x: 50 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ duration: 1 }}
+              {...fadeLeft}
             >
               HONG
             </motion.h1>
           </div>
 
           {/* Details */}
-          <div className='px-4 mt-8 text-center'>
+          <motion.div 
+            className='px-4 mt-8 text-center'
+            {...fadeRight}
+          >
             <p className='text-[2vw] md:text-[1vw] uppercase tracking-widest'>
               Software Developer / <br /> Creative Technologist
             </p>
             <a
               href="#contact"
-              className="mt-4 mb-4 inline-block border border-white md:border-black text-[2vw] md:text-[1vw] px-2 py-1 tracking-widest uppercase text-white md:text-black hover:bg-black hover:text-white transition rounded-sm"
+              className="mt-4 mb-4 inline-block border border-white md:border-black text-[2vw] md:text-[1vw] px-2 py-1 tracking-widest uppercase text-white md:text-[#2c2c2c] hover:bg-black hover:text-white transition rounded-sm"
             >
               Get in Touch
             </a>
-          </div>
+          </motion.div>
         </div>
 
         {/* Image */}
         <motion.div 
           className='relative hidden md:block w-full md:w-[50vw] md:h-[50vh] md:aspect-[4/5] border rounded-sm'
           style={{y}}
+          {...fadeIn}
         >
           <Image
             src="/images/me.jpg"
